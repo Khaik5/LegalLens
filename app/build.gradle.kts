924aa10557019052e318befd8796450ab0e3e306
Khaik5 <vietkhai14012005@gmail.com>
2026-08-26 15:53:14 +0700

    Project

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.lagallens"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.lagallens"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0"
+    )
+
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.1"
+    )
+
+    implementation(
+        "com.airbnb.android:lottie:6.7.0"
+    )
+
+    implementation(
+        "com.github.bumptech.glide:glide:5.0.5"
+    )
+
+    implementation(
+        "com.squareup.retrofit2:retrofit:3.0.0"
+    )
+
+    implementation(
+        "com.squareup.retrofit2:converter-gson:3.0.0"
+    )
+
+    implementation(
+        "com.squareup.okhttp3:okhttp:4.12.0"
+    )
+
+    implementation(
+        "com.squareup.okhttp3:logging-interceptor:4.12.0"
+    )
+
+    coreLibraryDesugaring(
+        "com.android.tools:desugar_jdk_libs:2.0.4"
+    )
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+}
